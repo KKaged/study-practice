bac9449b3bffda903d383b25cd972d1a82293cd4
KKaged <dhigaredaleon707@gmail.com>
2023-07-14 21:18:34 +0900

    Added a greetings Module Pattern.

--- Module Function/index.js
+++ Module Function/index.js
@@ -14,3 +14,14 @@ console.log(calculator.add(3, 7));
 console.log(calculator.sub(7, 2));
 console.log(calculator.mul(7, 3));
 console.log(calculator.div(14, 2));
+
+const greetings = (() => {
+  const person = "John";
+  const hello = `Hello, ${person}`;
+  return {
+    person,
+    hello,
+  };
+})();
+console.log(greetings.hello); // Output: "Hello, John"
+console.log(person); //Cannot access the variable.
