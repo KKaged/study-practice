0f3d117f37cfa248dc48ee70406b18e1c2b28f25
KKaged <dhigaredaleon707@gmail.com>
2023-08-23 21:34:14 +0900

    Studied more classes.

--- classes/script.js
+++ classes/script.js
@@ -1,12 +1,37 @@
 class User {
-  constructor(name) {
+  constructor(name, age, location) {
     this.name = name;
+    this.age = age;
+    this.location = location;
   }
 
   greetings() {
-    console.log(this.name);
+    console.log(
+      `Hello ${this.name}, You are ${this.age} years old. You currently live in ${this.location}`
+    );
   }
 }
 
-let user = new User("John");
+let user = new User("John", 21, "Los Angeles, California");
+let user1 = new User("Jose", 37, "Moscow, Russia");
 user.greetings();
+user1.greetings();
+
+class person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    if (value.length < 4) {
+      console.log("Name is too short");
+      return;
+    }
+    return (this._name = value);
+  }
+}
+let profile = new person("John");
+console.log(profile.name);
