c095509ebfe93cae5e8f07f6d42b064571f92e57
KKaged <dhigaredaleon707@gmail.com>
2023-10-18 18:23:12 +0900

    Studied and reviewed lexical scopes

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,17 @@
-function throwDice(num) {
-  console.log(` You threw a ${Math.floor(Math.random() * num) + 1}!`);
-}
+// function throwDice(num) {
+//   console.log(` You threw a ${Math.floor(Math.random() * num) + 1}!`);
+// }
 
-throwDice(999);
+// throwDice(999);
+
+//Everything under her is about scope
+
+let global = `I'm a global variable!`;
+function private() {
+  let priv = `I'm a private variable!`;
+  console.log(priv);
+}
+private(); //Output is "I'm a private variable"
+console.log(global); //Output is "I'm a global variable"
+
+console.log(priv); //Undefined
