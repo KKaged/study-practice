fa16f961cbd3f1fae0529802a4dca2e91a3e2ea8
KKaged <dhigaredaleon707@gmail.com>
2023-10-19 18:59:49 +0900

    studied filter method

--- functions/index.js
+++ functions/index.js
@@ -178,3 +178,14 @@ const id = setInterval(() => {
 }, 300);
 
 clearInterval(id);
+
+// Filter
+
+const number = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
+const pick2 = number.filter((n) => {
+  return n % 2 === 1; // Filters out everything that is false and places everything that is true into a new array , "pick2"
+});
+console.log(pick2); //[1,  3,  5,  7,  9, 11, 13, 15, 17, 19]
